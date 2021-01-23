86dc0eef3885ff456702865163465a613b04ee01
Joan0018 <joanhau201018@hotmail.com>
2021-01-23 16:49:34 +0800

    Run output in emulator
    W1 Lesson 2

---
+++ Week 1/AndroidBasicsCreateyourfirstAndroidapp/app/src/main/java/com/example/android_basics_create_your_first_android_app/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.android_basics_create_your_first_android_app
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
